7f4e6944eb36e73702dcf19c5537dbaa6fbd9800
Oritseweyinmi Samuel Tuoyo <samueltuoyo9082@gmail.com>
2024-12-04 18:23:17 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import authRoutes from './routes/authRoutes.js';
+import createAgentRoute from './routes/createAgentRoute.js';
 import cors from 'cors';
 const app = express();
 
@@ -9,9 +10,10 @@ app.use(bodyParser.json());
 app.use(cors())
 
 app.use('/auth', authRoutes);
+app.use('/assistants', createAgentRoute)
 
 app.get('/', (req, res) => {
-    res.status(200).send('Firebase Integration Working!');
+    res.status(200).send('Welcome to Nexus Botix Official Backend');
 });
 
 
